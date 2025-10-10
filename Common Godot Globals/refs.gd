a2ed04816e0bcb7d09490281da9d606849b253ac
James Krakora <krakorajames@gmail.com>
2025-10-10 10:54:32 -0500

    Create refs.gd

---
+++ Common Godot Globals/refs.gd
@@ -0,0 +1,27 @@
+extends Node
+
+# --= Documentation =--
+# Written by: James Krakora
+# Function: Holds references to colors, icons, scenes, 
+# etc. with many many @export variables
+# --=================--
+
+# === Variables ===
+# --- Signals ---
+# --- Enums ---
+# --- Constants ---
+# --- @export ---
+# --- Regular ---
+# --- @onready ---
+
+# === Functions ===
+#func _init() -> void:
+#func _enter_tree() -> void:
+#func _ready() -> void:
+#func _unhandled_input(event: InputEvent) -> void:
+#func _process(delta: float) -> void:
+#func _physics_process(delta: float) -> void:
+# --- Public Funcs ---
+# --- Private Funcs ---
+# --- Subclasses ---
+# End of Script
