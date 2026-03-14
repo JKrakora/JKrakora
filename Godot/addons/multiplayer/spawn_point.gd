b0e0db103c19af0c38102e2f324a6aa1844ec899
James Krakora <krakorajames@gmail.com>
2026-03-14 10:20:31 -0500

    Refactor SpawnPoint to manage occupancy state
    Removed time tracking and is_occupied function.

--- Godot/addons/multiplayer/spawn_point.gd
+++ Godot/addons/multiplayer/spawn_point.gd
@@ -1,22 +1,17 @@
 class_name SpawnPoint
 extends Node3D
 
-var time : float = 3.0
-var assumed_unoccupied_time : float = 3.0
+var is_occupied : bool
 
 func _process(delta: float) -> void:
 	time += delta
 
 
 func get_info() -> Vector4:
-	time = 0
+	is_occupied = true
 	return Vector4(
 		global_position.x,
 		global_position.y,
 		global_position.z,
 		rotation.y,
 	)
-
-
-func is_occupied() -> bool:
-	return time <= assumed_unoccupied_time
