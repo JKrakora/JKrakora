0c8fc9d02177d5f68f6095e74ecc00977a329a0e
James Krakora <krakorajames@gmail.com>
2026-03-14 13:20:28 -0500

    Refactor cursor containment handling in Copy-Paste Code

--- Godot/Drag n Drop/Copy-Paste Code.gd
+++ Godot/Drag n Drop/Copy-Paste Code.gd
@@ -10,6 +10,13 @@ func _unhandled_input(event : InputEvent) -> void:
 	    $Camera3D.rotation_degrees.x = clampf($Camera3D.rotation_degrees.x, -89, 89)
 
 
+func _handle_cursor_containment(event : InputEvent) -> void:
+    if event.is_action_pressed("ui_cancel") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseButton and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE and not is_cursor_being_freed:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
 var is_cursor_being_freed := false
 func _handle_cursor_containment_with_alt(event : InputEvent) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and event.is_action_pressed("free_cursor"):
@@ -17,13 +24,6 @@ func _handle_cursor_containment_with_alt(event : InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if is_cursor_being_freed and event.is_action_released("free_cursor"):
 		is_cursor_being_freed = false
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-
-func _handle_cursor_containment(event : InputEvent) -> void:
-    if event.is_action_pressed("ui_cancel") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if event is InputEventMouseButton and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE and not is_cursor_being_freed:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
