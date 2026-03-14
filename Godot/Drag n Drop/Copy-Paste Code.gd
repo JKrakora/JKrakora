98681642882ef07f4102cf552b972b9f90084f9f
James Krakora <krakorajames@gmail.com>
2026-03-14 13:16:46 -0500

    Create Copy-Paste Code.gd

---
+++ Godot/Drag n Drop/Copy-Paste Code.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var is_cursor_being_freed := false
+func _handle_cursor_containment_with_alt(event : InputEvent) -> void:
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and event.is_action_pressed("free_cursor"):
+		is_cursor_being_freed = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if is_cursor_being_freed and event.is_action_released("free_cursor"):
+		is_cursor_being_freed = false
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _handle_cursor_containment() -> void:
+    if event.is_action_pressed("ui_cancel") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseButton and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE and not is_cursor_being_freed:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _handle_camera_movement() -> void:
+    if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+	    rotation_degrees.y -= event.relative.x * camera_sensitivity
+	    $Camera3D.rotation_degrees.x -= event.relative.y * camera_sensitivity
+	    $Camera3D.rotation_degrees.x = clampf($Camera3D.rotation_degrees.x, -89, 89)
