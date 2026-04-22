f47a126de3000255eea28b80ce1af9bbac065292
James Krakora <krakorajames@gmail.com>
2026-04-22 09:54:26 -0500

    Add camera movement with freeable cursor functionality

--- Godot/Drag n Drop/Copy-Paste Code.gd
+++ Godot/Drag n Drop/Copy-Paste Code.gd
@@ -1,3 +1,4 @@
+## Regular Camera movement and Cursor containment
 var camera_sensitivity:= 0.1
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
@@ -11,9 +12,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		$Camera3D.rotation_degrees.x = clampf($Camera3D.rotation_degrees.x, -89, 89)
 
 
+## Camera Movement with Freeable Cursor containment
+## Requires "free_cursor" InputEvent
 var camera_sensitivity:= 0.1
 var is_cursor_being_freed := false
 func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseButton and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE and not is_cursor_bring_freed:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and event.is_action_pressed("free_cursor"):
 		is_cursor_being_freed = true
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
