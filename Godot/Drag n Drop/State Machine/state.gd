ed72e5f83252eed93082dd7cd2329774978045e4
James Krakora <krakorajames@gmail.com>
2026-05-19 12:03:30 -0500

    Update state.gd

--- Godot/Drag n Drop/State Machine/state.gd
+++ Godot/Drag n Drop/State Machine/state.gd
@@ -1,23 +1,8 @@
 class_name State
 extends Node
-
 var user
-
-func enter() -> void:
-	print("%s: Entering %s" % [user.name, name])
-
-
-func exit() -> void:
-	print("%s: Exiting %s" % [user.name, name])
-
-
-func process_input(event: InputEvent) -> State:
-	return null
-
-
-func process_frame(delta: float) -> State:
-	return null
-
-
-func process_physics(delta: float) -> State:
-	return null
+func enter() -> void: pass
+func exit() -> void: pass
+func process_input(event: InputEvent) -> State: return null
+func process_frame(delta: float) -> State: return null
+func process_physics(delta: float) -> State: return null
