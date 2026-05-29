a88cd44cd499b4bf9214225c204d51b167c4dd8e
James Krakora <krakorajames@gmail.com>
2026-05-29 12:55:36 -0500

    Add camera sensitivity and input handling

--- Godot/systems/smooth_camera/smooth_camera.gd
+++ Godot/systems/smooth_camera/smooth_camera.gd
@@ -7,6 +7,18 @@ func _ready() -> void:
 	top_level = true
 	physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_ON
 
+var camera_sensitivity:= 0.1
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseButton and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotation_degrees.y -= event.relative.x * camera_sensitivity
+		rotation_degrees.x -= event.relative.y * camera_sensitivity
+		rotation_degrees.x = clampf(rotation_degrees.x, -89, 89)
+
 
 func _process(_delta: float) -> void:
 	global_position = target.get_global_transform_interpolated().origin
