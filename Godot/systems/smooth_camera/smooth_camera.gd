4edf299d9dbd0b12501eb5d67b16bfc06da6776a
James Krakora <krakorajames@gmail.com>
2026-05-30 17:37:21 -0500

    Refactor camera sensitivity variable and usage

--- Godot/systems/smooth_camera/smooth_camera.gd
+++ Godot/systems/smooth_camera/smooth_camera.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
 	top_level = true
 	physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_ON
 
-var camera_sensitivity:= 0.1
+
+@export var sensitivity:= 0.1
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -15,10 +16,11 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotation_degrees.y -= event.relative.x * camera_sensitivity
-		rotation_degrees.x -= event.relative.y * camera_sensitivity
+		rotation_degrees.y -= event.relative.x * sensitivity
+		if target: target.rotation.y = self.rotation.y
+		rotation_degrees.x -= event.relative.y * sensitivity
 		rotation_degrees.x = clampf(rotation_degrees.x, -89, 89)
 
 
 func _process(_delta: float) -> void:
-	global_position = target.get_global_transform_interpolated().origin
+	if target: global_position = target.get_global_transform_interpolated().origin
