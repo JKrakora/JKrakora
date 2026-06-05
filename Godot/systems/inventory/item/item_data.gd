d002005c6487f015f21a9fc5dfbab0ec353147fb
James Krakora <krakorajames@gmail.com>
2026-06-05 10:24:18 -0500

    Delete Godot/systems/inventory directory

--- Godot/systems/inventory/item/item_data.gd
+++
@@ -1,9 +0,0 @@
-class_name ItemData
-extends Resource
-
-@export var ID: int
-@export var name: String
-@export var icon: Texture2D
-
-func _to_string() -> String:
-	return "%s" % name
--- Godot/systems/inventory/stackable_item/stackable_item_data.gd
+++
@@ -1,8 +0,0 @@
-class_name StackableItemData
-extends ItemData
-
-@export var amount:= 1
-@export var max_amount:= 9999
-
-func _to_string() -> String:
-	return "%s:(%s/%s)" % [ID, amount, max_amount]
