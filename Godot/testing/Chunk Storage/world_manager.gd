e5d05702bac4b35d0769164499456db336a520ac
James Krakora <krakorajames@gmail.com>
2026-09-05 00:09:02 -0500

    Add files via upload

--- Godot/testing/Chunk Storage/world_manager.gd
+++ Godot/testing/Chunk Storage/world_manager.gd
@@ -1,20 +1,32 @@
 extends Node3D
 
-@export var world_chunk_count:= Vector3i(1, 1, 1)
-@export var chunk_voxel_count: int= 8
+
+## === World/Stack/Chunk/Voxel ===
+@export var world_chunk_count:= Vector3i(50, 10, 50)
 var world_size_logical: Vector3i
-var chunk_atlas: Dictionary[Vector3i, ChunkData]
+
+@export var chunk_voxel_count: int= 8
+var chunk_atlas: Array[ChunkData]
+var active_chunks: Dictionary[Vector3i, Chunk]
+
+@export var voxel_size_physical: float= 1.0
 
 func _ready() -> void:
 	_fill_atlas()
 
 
 func _fill_atlas() -> void:
+	var noise = Noise4D.new()
+	## setup noise params here
+	
+	chunk_atlas.resize(world_chunk_count.x * world_chunk_count.y * world_chunk_count.z)
 	world_size_logical = world_chunk_count * chunk_voxel_count
 	
 	for cx in range(world_chunk_count.x):
 		for cy in range(world_chunk_count.y):
 			for cz in range(world_chunk_count.z):
+				var chunk_coord:= Vector3i(cx, cy, cz)
+				var chunk_offset:= chunk_coord * chunk_voxel_count
 				var data = ChunkData.new()
 				
 				var densities: Array[float] = []
@@ -23,12 +35,26 @@ func _fill_atlas() -> void:
 				for x in range(chunk_voxel_count):
 					for y in range(chunk_voxel_count):
 						for z in range(chunk_voxel_count):
-							densities[index] = index
+							var voxel_coord = Vector3i(x, y, z) + chunk_offset
+							var theta_x = TAU * voxel_coord.x / world_size_logical.x
+							var theta_z = TAU * voxel_coord.z / world_size_logical.z
+							var torus_coord = Vector4(
+								cos(theta_x),
+								sin(theta_x),
+								cos(theta_z),
+								sin(theta_z),
+							) * 50.0
+							
+							var terrain_value = noise.get_noise_4dv(torus_coord)
+							terrain_value -= (float(voxel_coord.y) / world_size_logical.y) * 2.0 - 1.0
+							
+							## Other Noise logic here
+							
+							densities[index] = terrain_value
 							index += 1
-				
 				data.voxel_densities = densities
 				data.edge_voxel_count = chunk_voxel_count
-				chunk_atlas[Vector3i(cx, cy, cz)] = data
+				chunk_atlas[get_atlas_index(chunk_coord)] = data
 
 
 func get_chunk_data_for_meshing(coord: Vector3i) -> PackedFloat32Array:
@@ -46,8 +72,8 @@ func get_chunk_data_for_meshing(coord: Vector3i) -> PackedFloat32Array:
 	var xy_edge = current.get_xy_edge() ## of (1, 1, 0)
 	var xyz_corner = current.get_xyz_corner() ## of (1, 1, 1)
 	
-	var indices = [0, 0, 0, 0, 0, 0, 0, 0]
-	#indices.resize(8)
+	var indices: Array[int] = []
+	indices.resize(8)
 	
 	## Look...this is a fucking mess...but I needed a way to interweave
 	## neighboring chunk densities since I'm using marching cubes and it 
@@ -86,8 +112,9 @@ func get_chunk_data_for_meshing(coord: Vector3i) -> PackedFloat32Array:
 	
 	return PackedFloat32Array(data)
 
+
 func get_atlas_chunk(coord: Vector3i) -> ChunkData:
-	return chunk_atlas[get_atlas_coord(coord)]
+	return chunk_atlas[get_atlas_index(get_atlas_coord(coord))]
 
 
 func get_atlas_coord(coord: Vector3i) -> Vector3i:
@@ -96,3 +123,7 @@ func get_atlas_coord(coord: Vector3i) -> Vector3i:
 		coord.y % world_chunk_count.y,
 		coord.z % world_chunk_count.z,
 	)
+
+
+func get_atlas_index(atlas_coord: Vector3i) -> int:
+	return atlas_coord.x + (atlas_coord.y * world_chunk_count.x) + (atlas_coord.z * world_chunk_count.x * world_chunk_count.y)
